be1fbbc78ea2bdfd9521e08c7aaafb6e6848a21f
Josef.Pinkr <pinkr@email.cz>
2025-10-23 18:13:06 +0200

    table change

--- dwh4cicd_vscode/Network/Tables/dim_User.sql
+++ dwh4cicd_vscode/Network/Tables/dim_User.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [Network].[dim_User] (
-    [UserId]    INT           NOT NULL,
-    [UserName]  VARCHAR (50)  NOT NULL,
-    [City]      VARCHAR (50)  NOT NULL,
-    [CreatedAt] DATETIME2 (0) NOT NULL
+    [UserId]   INT           NOT NULL,
+    [UserName] VARCHAR (50)  NOT NULL,
+    [Email]    VARCHAR (100) NOT NULL
 );
 
 
